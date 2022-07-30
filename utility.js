fff60e6be3c60b0707cbde89cb7ff2e3399387d6
unknown <save_kappom@hotmail.com>
2022-07-30 17:59:32 +0700

    codelab5

--- utility.js
+++ utility.js
@@ -1,12 +1,13 @@
 //utility.js
 
 const request = require("request-promise");
-const EXTERNAL_API=""; // put url
-const accessToken=""; // put access token
+const EXTERNAL_API="https://tools.ecpe.nu.ac.th/network/api/student/"; // put url
+const accessToken="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC90b29scy5lY3BlLm51LmFjLnRoXC9uZXR3b3JrXC9hcGlcL2xvZ2luIiwiaWF0IjoxNjU5MTc3ODE1LCJleHAiOjE2NTkxODE0MTUsIm5iZiI6MTY1OTE3NzgxNSwianRpIjoiUktrcHpXUXJ5dG9JeW8xYSIsInN1YiI6OTMsInBydiI6IjIzYmQ1Yzg5NDlmNjAwYWRiMzllNzAxYzQwMDg3MmRiN2E1OTc2ZjcifQ.pu3_5olwxdYoLLJGgqOqZ_oaEgrgK68wUnWYnxv7B08"; // put access token
 const student = {
-  name: '', // replace with your full name.
-  age: 19, // put your age.
-  gender: '', // replace with your gender
+  student_id: 63362587,
+  name: 'Thanaphat Jegwong', // replace with your full name.
+  age: 20, // put your age.
+  gender: 'Male', // replace with your gender
   department: 'CPE'
 };
 
@@ -14,14 +15,10 @@ const student = {
 exports.findStudentbyId = function (student_id, cb) {
   //-- call external api 
           request({
-            method: "POST",
-            uri: EXTERNAL_API,
+            method: "GET",
+            uri: EXTERNAL_API+student_id,
             headers: {
                 Authorization: `Bearer ${accessToken}`
-            },
-            formData: {
-                message: `HTTP Request :${data.student_id} `,
-                student_id: student_id
             }
         }).then((response) => {
             console.log('Sent');
